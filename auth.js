9761f89a4e86e2a68e6d219ac15e17de272bc090
Mark <mark.capili@ada.asia>
2024-05-24 15:10:22 +0800

    protected routes, JWT

---
+++ auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+export default authenticate;
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
-
 import runScraperProcess from "./scraper.js";
+import dotenv from "dotenv";
+import authenticate from "./auth.js";
+import jwt from "jsonwebtoken";
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.get("/", async (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", authenticate, async (req, res) => {
   res.send(`
     <html>
       <body>
@@ -16,9 +22,21 @@ app.get("/", async (req, res) => {
   console.log(`I got pinged`);
 });
 
-app.get("/run", async (req, res) => {
+app.get("/run", authenticate, async (req, res) => {
   await runScraperProcess();
   res.send("Function run successfully!");
+});
+
+app.post("/login", (req, res) => {
+  const username = process.env.USER_ENV;
+  const password = process.env.PASSWORD_ENV;
+  console.log(req.body);
+  if (req.body.username === username && req.body.password === password) {
+    const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
+    res.json({ token });
+  } else {
+    res.status(401).send("Invalid username or password");
+  }
 });
 
 app.listen(port, () => {
